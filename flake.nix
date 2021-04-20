547e3a94754b96b4651ea7b564339fb94282a89e
Sergey Zubkov <ksevelyar@gmail.com>
2021-04-20 21:53:39 +0300

    template for a rust/flakes/nix project

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    rust-overlay.url = github:oxalica/rust-overlay;
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+      in
+        {
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              (
+                rust-bin.stable.latest.default.override {
+                  targets = [ "thumbv7m-none-eabi" ];
+                }
+              )
+              gcc-arm-embedded
+              openocd
+              pkg-config
+              libusb
+            ];
+          };
+        }
+    );
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Incubator
+
+1. `nix develop`
+2. `cargo flash --chip stm32f103C8 --release`
